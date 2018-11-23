94c81e8ad1de9109fd5f3a4ffe168b6ae5476a93
KentAgent <georgios.apostolidis92@gmail.com>
2018-11-23 15:34:33 +0100

    Added some more commands to Program.cs

--- OOAD-Labb1-Calculator/Program.cs
+++ OOAD-Labb1-Calculator/Program.cs
@@ -8,9 +8,19 @@ namespace OOADLabb1Calculator
         {
             ElectronicDevice newDevice = CalculatorDevice.GetDevice();
 
+            // Turn Device On
+
             OnCommand onCommand = new OnCommand(newDevice);
 
             DeviceButton onPressed = new DeviceButton(onCommand);
+
+            onPressed.press();
+
+            // Turn Device Off
+
+            OffCommand offCommand = new OffCommand(newDevice);
+
+            onPressed = new DeviceButton(offCommand);
 
             onPressed.press();
         }
