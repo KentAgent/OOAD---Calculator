39a45d0cf827f993d48580d53c78caf5f8a11ec6
William Lennartsson <william.j.lennartsson@gmail.com>
2018-11-27 22:50:13 +0100

    Sick stack

---
+++ OOAD-Labb1-Calculator/StackHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace OOADLabb1Calculator
+{
+    class StackHandler
+    {
+        public StackHandler()
+        {
+            _stack = new Stack();
+        }
+        Stack _stack;
+        Stack Stack
+        {
+            get
+            {
+                return _stack;
+            }
+        }
+        public bool Insert(Command command)
+        {
+            return true;
+        }
+    }
+}
